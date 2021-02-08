02221bdb6abfffa93b497c2de079919aa2cf079e
Sooraj <srj7@users.noreply.github.com>
2021-02-08 23:21:23 +0530

    Update help.js

--- handlers/help.js
+++ handlers/help.js
@@ -5,12 +5,10 @@ const Tf = require('telegraf').Composer;
 // @ts-ignore
 const pkg = require('../package.json');
 
-const helpText = ({ me }) => `Execute code.
+const helpText = ({ me }) => `Hey there, I can help you compile code snippets in telegram.
 
-Usage: \`/<language> <code> [/stdin <stdin>]\`
-
-Inline mode:
-\`@${me} <language> <code> [/stdin <stdin>]\`
+How to use: \`/<language> <code> [/stdin <stdin>]\`
+example : \`/python print("Hello World") /stdin inputs(if any) \`
 
 Line breaks and indentation are supported.
 
